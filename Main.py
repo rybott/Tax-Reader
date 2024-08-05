e072fee958cc6665dd88c34a50e7b1dc76c9534a
Ryan B. <rybott5000.5@gmail.com>
2024-08-05 02:17:27 -0400

    f

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
 import pytesseract
+import PIL.Image
 import cv2 as cv
+
+myconfig = r"--psm 4"
+
+text = pytesseract.image_to_string(PIL.Image.open(r"C:\Users\rybot\OneDrive\Desktop\Taxes for Code\111\Image Versions\Image 111 S Corp Tax Return IS no Statements.jpg"), config=myconfig)
+print(text)
 
